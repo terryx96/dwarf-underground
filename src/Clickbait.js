4ea3b4cbc6368289da818de1a607a84666a1b814
Terry Wade <terry.wade@valpo.edu>
2018-06-11 23:28:22 -0400

    Render ads through a map function

--- src/Clickbait.js
+++ src/Clickbait.js
@@ -1,22 +1,9 @@
 import React, {Component} from 'react';
 
 class Clickbait extends Component{
-    constructor(){
-        super();
-        this.state = {
-            ads: [
-                {name: "Single Orcs in Indianapolis", source: "https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif", alt: "orc"},
-                {},
-                {},
-                {},
-            ],
-        }
-    }
-
     render(){
         return(
             <div className="small-6 medium-3 columns other-article">
-
                 <a href="#">
                     <img src={this.props.source} alt={this.props.alt} />
                     <p>{this.props.text}</p>
--- src/Main.js
+++ src/Main.js
@@ -7,6 +7,17 @@ import Clickbait from './Clickbait';
 import Comment from './Comment';
 
 class Main extends Component{
+    constructor(){
+        super();
+        this.state = {
+            ads: [
+                {name: "Single Orcs in Indianapolis", source: "https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif", alt: "orc"},
+                {name: "You won't believe what's under this mountain", source: "https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg", alt: "mountain"},
+                {name: "Mine 20% more gold with One Weird Trick", source: "http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg", alt: "gold"},
+                {name: "Surprise for Indiana Hobbits born before 1999", source: "http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg", alt: "hobbit"},
+            ],
+        }
+    }
     render(){
         return(
             <main className="expanded row">
@@ -22,18 +33,12 @@ class Main extends Component{
 
                 <div className="small-12 columns other-articles">
                     <h2>From around the Realm</h2>
-                    <Clickbait text = "Single Orcs in Indianapolis" alt = "orc" source = "https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" />
-                    <Clickbait text = "You won't believe what's under this mountain" alt = "mountain" source = "https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" />
-                    <Clickbait text = "Mine 20% more gold with One Weird Trick" alt = "gold" source = "http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" />
-                    <Clickbait text = "Surprise for Indiana Hobbits born before 1999" alt = "hobbit" source = "http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" />
-                </div>
+                    {this.state.ads.map(ad => <Clickbait text = {ad.name} source = {ad.source} alt = {ad.alt} />)}
+                 </div>
             </main>
         );
     }
 
-    comment(){
-        alert("hey");
-    }
 }
 
 export default Main;
